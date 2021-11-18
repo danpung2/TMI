d737a13c9e6683c0619a4b378bf2f3504a6be448
danpung2 <danpung_2@naver.com>
2021-11-18 17:09:06 +0900

    Create join activity

---
+++ app/src/main/java/com/example/tmi/JoinActivity.java
@@ -0,0 +1,4 @@
+package com.example.tmi;
+
+public class JoinActivity {
+}
--- app/src/main/java/com/example/tmi/MainActivity.java
+++ app/src/main/java/com/example/tmi/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.tmi;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Intent moveToJoin = new Intent(this, JoinActivity.class);
+        Intent moveToLogin = new Intent(this, LoginActivity.class);
+
+        Button joinBtn = findViewById(R.id.joinBtn);
+        joinBtn.setOnClickListener(v -> {
+            startActivity(moveToJoin);
+            finish();
+        });
+
+        Button loginBtn = findViewById(R.id.loginBtn);
+        loginBtn.setOnClickListener(v -> {
+            startActivity(moveToLogin);
+            finish();
+        });
+
     }
 }
