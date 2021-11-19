7e339796d10291788cbfe7c8cebd0e0ea1b01e04
Cho Hyunseo <danpung_2@naver.com>
2021-11-19 23:01:56 +0900

    FEAT join

--- app/src/main/java/com/example/tmi/JoinActivity.java
+++ app/src/main/java/com/example/tmi/JoinActivity.java
@@ -1,5 +1,7 @@
 package com.example.tmi;
 
+import static android.content.ContentValues.TAG;
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
@@ -22,53 +24,49 @@ import com.google.firebase.database.FirebaseDatabase;
 import java.util.HashMap;
 
 public class JoinActivity extends AppCompatActivity {
-    private FirebaseAuth firebaseAuth;
+    private FirebaseAuth mAuth;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_join);
+        mAuth = FirebaseAuth.getInstance();
 
         EditText email = findViewById(R.id.email);
         EditText password = findViewById(R.id.password);
 
         Button joinBtn = findViewById(R.id.joinBtn);
 
+
         joinBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
-                //파이어베이스에 신규계정 등록하기
-                firebaseAuth.createUserWithEmailAndPassword(email.toString(), password.toString()).addOnCompleteListener(JoinActivity.this, new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if (task.isSuccessful()) {
-                            FirebaseUser user = firebaseAuth.getCurrentUser();
-                            String email = user.getEmail();
-
-                            //해쉬맵 테이블을 파이어베이스 데이터베이스에 저장
-                            HashMap<Object, String> hashMap = new HashMap<>();
-                            hashMap.put("email", email);
-
-                            FirebaseDatabase database = FirebaseDatabase.getInstance();
-                            DatabaseReference reference = database.getReference("Users");
-
-                            Intent intent = new Intent(JoinActivity.this, StartActivity.class);
-                            startActivity(intent);
-                            finish();
-                            Toast.makeText(JoinActivity.this, "회원가입에 성공하셨습니다.", Toast.LENGTH_SHORT).show();
-
-                        } else {
-                            Toast.makeText(JoinActivity.this, "이미 존재하는 아이디 입니다.", Toast.LENGTH_SHORT).show();
-                            return;  //해당 메소드 진행을 멈추고 빠져나감.
-
-                        }
-
-                    }
-                });
-
+                createAccount(email.getText().toString().trim(), password.getText().toString().trim());
             }
         });
 
+    }
+    public void createAccount(String email, String password) {
+        // [START create_user_with_email]
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "createUserWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            // startActivity(moveToStart);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                            Toast.makeText(JoinActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                            return;
+                        }
+                    }
+                });
+        // [END create_user_with_email]
     }
 }
