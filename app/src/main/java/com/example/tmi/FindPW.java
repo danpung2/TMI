4fd2cac5d4efe15de4385d83d1aceeab1eaaf92a
Cho Hyunseo <danpung_2@naver.com>
2021-12-15 20:22:30 +0900

    FEAT find password

--- app/src/main/java/com/example/tmi/FindPW.java
+++ app/src/main/java/com/example/tmi/FindPW.java
@@ -1,17 +1,63 @@
 package com.example.tmi;
 
+import static android.content.ContentValues.TAG;
+
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+
 public class FindPW extends AppCompatActivity {
+    private FirebaseAuth mAuth;
+    private String userEmail;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_findpw);
 
+        EditText email = findViewById(R.id.email);
+        Button sendBtn = findViewById(R.id.sendBtn);
 
+        sendBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                userEmail = email.getText().toString().trim();
+                sendPasswordReset(userEmail);
+            }
+        });
+
+    }
+
+    public void sendPasswordReset(String email){
+        FirebaseAuth auth = FirebaseAuth.getInstance();
+
+        auth.sendPasswordResetEmail(email)
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if(task.isSuccessful()){
+                            Log.d(TAG, "Email sent");
+                            StartToast(R.string.email_sent);
+                        }
+
+                    }
+                });
+    }
+
+    public void StartToast(Integer msg){
+        Toast toast = Toast.makeText(FindPW.this, msg, Toast.LENGTH_SHORT);
+        toast.setGravity(Gravity.TOP, 0, 200);
+        toast.show();
     }
 }
